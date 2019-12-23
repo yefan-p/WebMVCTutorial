60da9d5c481e52766c6dee1c478b78b5fcfe87b9
Efanov P.A <efanov.pa.a@gmail.com>
2019-12-23 10:07:46 +0300

    Edit route config

--- WebMVCTutorial/App_Start/RouteConfig.cs
+++ WebMVCTutorial/App_Start/RouteConfig.cs
@@ -13,11 +13,17 @@ namespace WebMVCTutorial
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Contact", "Contact/{*pathInfo}",
+                new { controller = "Home", action = "Contact" });
+
+            routes.MapRoute("About", "About/{*pathInfo}",
+                new { controller = "Home", action = "About" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            ) ;
         }
     }
 }
